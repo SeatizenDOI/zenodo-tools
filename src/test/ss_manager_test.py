194aabaabd3a6b2aa6f526d12e4022bafbff8744
Gouderg <victor.illien974@gmail.com>
2024-07-09 15:51:10 +0400

    Add some test

--- src/test/ss_manager_test.py
+++ src/test/ss_manager_test.py
@@ -39,6 +39,23 @@ class TestSessionManager(unittest.TestCase):
         ]
         common_parent_folder = "DCIM"
         self.assertEqual(session.get_frame_parent_folder(list_frames_parent), common_parent_folder)
+    
+    def test_constructor_1(self):
+        session = SessionManager("20010101_FRA-BREST_ASV-01_01", TMP_PATH)
+
+        self.assertEqual(session.place, "Brest")
+        self.assertEqual(session.country, "France")
+        self.assertEqual(session.date, "2001-01-01")
+        self.assertEqual(session.platform, "ASV")
+    
+    def test_constructor_2(self):
+        session = SessionManager("15471212_SYC-CAP-HORNE-BLEU_UAV_01", TMP_PATH)
+
+        self.assertEqual(session.place, "Cap-Horne-Bleu")
+        self.assertEqual(session.country, "Seychelles")
+        self.assertEqual(session.date, "1547-12-12")
+        self.assertEqual(session.platform, "UAV")
+
 
 if __name__ == "__main__":
     unittest.main()
